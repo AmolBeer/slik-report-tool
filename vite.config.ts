1ff1451a80b693b63f20e4add65b4d214cfb05fc
unknown <duhuanhuan01@vcredit.cn>
2026-06-05 22:57:42 +0800

    Trigger new deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/slik-report-tool/',
+  // Deploy to GitHub Pages
   server: {
     port: 3000,
     open: true
